430ce7c19faf1fa994912a1fdb096303c6383a56
Andrzej Skrodzki <andrzej.skrodzki@gmail.com>
2015-06-20 19:56:16 +0200

    Add multiple runs

--- docker-benchmarks/run.sh
+++ docker-benchmarks/run.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 echo $BENCH
-if [ "$BENCH" = "ab1" ]; then
-    ab -c ${CL} -n ${NR} -k -e /logs/sample.csv ${HOST} > /logs/sample.out
-elif [ "$BENCH" = "ab2" ]; then
-    ab -c ${CL} -n ${NR} -e /logs/sample.csv ${HOST} > /logs/sample.out
-elif [ "$BENCH" = "mysql" ]; then
-    sysbench --test=oltp --mysql-host=${MHOST} \
-    --mysql-port=${MPORT} --oltp-table-size=1000000 \
-    --mysql-db=test --mysql-user=admin --mysql-password=test1234 \
-    --max-time=${MTIME} --oltp-read-only=on --max-requests=0  \
-    --num-threads=8 run
-elif [ "$BENCH" = "cpu" ]; then
-    sysbench --test=cpu --cpu-max-prime=${CPRIME} run
-elif [ "$BENCH" = "dacapo" ]; then
-	java -jar /dacapo.jar ${DCMD}
-fi
+for (( i = 0; i < ${COUNT}; i++ )); do
+	if [ "$BENCH" = "ab1" ]; then
+	    ab -c ${CL} -n ${NR} -k -e /logs/sample.csv ${HOST} > /logs/sample.out
+	elif [ "$BENCH" = "ab2" ]; then
+	    ab -c ${CL} -n ${NR} -e /logs/sample.csv ${HOST} > /logs/sample.out
+	elif [ "$BENCH" = "mysql" ]; then
+	    sysbench --test=oltp --mysql-host=${MHOST} \
+	    --mysql-port=${MPORT} --oltp-table-size=1000000 \
+	    --mysql-db=test --mysql-user=admin --mysql-password=test1234 \
+	    --max-time=${MTIME} --oltp-read-only=on --max-requests=0  \
+	    --num-threads=8 run
+	elif [ "$BENCH" = "cpu" ]; then
+	    sysbench --test=cpu --cpu-max-prime=${CPRIME} run
+	elif [ "$BENCH" = "dacapo" ]; then
+		java -jar /dacapo.jar ${DCMD}
+	fi
+done
