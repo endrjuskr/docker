779986c42f72e31765d0609f3e51fb611502fe3d
Andrzej Skrodzki <andrzej.skrodzki@gmail.com>
2015-08-11 16:27:54 +1000

    Fix apache2

--- docker-common/run.sh
+++ docker-common/run.sh
@@ -2,7 +2,11 @@
 
 cd apache
 chmod +x create_apache.sh
-./create_apache.sh 4 127.0.0.1
+sh create_apache.sh 4 127.0.0.1
+
+cat /etc/apache2-1/envvars
+cat /etc/apache2-1/ports.conf
+cat /etc/apache2-1/apache2.conf
 
 . /etc/apache2-1/envvars
 /usr/sbin/apache2ctl -d /etc/apache2-1/ -k start
