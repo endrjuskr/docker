6ad19bcf54594664a8ebd388fa508192ce5a7095
Andrzej Skrodzki <andrzej.skrodzki@gmail.com>
2015-08-11 17:07:14 +1000

    Fix apache.

--- docker-common/run.sh
+++ docker-common/run.sh
@@ -2,11 +2,11 @@
 
 cd apache
 chmod +x create_apache.sh
-sh create_apache.sh 4 127.0.0.1
+sh create_apache.sh 4 10.240.162.123
 
 . /etc/apache2-1/envvars
 /usr/sbin/apache2ctl -d /etc/apache2-1/ -k start
 
-curl http://127.0.0.1:86/index.php
+curl http://10.240.162.123:86/app1/index.php
 
 sleep infinity
