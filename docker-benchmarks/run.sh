cf6c1a5068d96dbd35d16a67657d32ad83213bc5
Andrzej Skrodzki <andrzej.skrodzki@gmail.com>
2015-08-11 12:48:40 +1000

    More logs.

--- docker-benchmarks/run.sh
+++ docker-benchmarks/run.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 for (( i = 0; i < ${COUNT}; i++ )); do
 	if [ "$BENCH" = "ab1" ]; then
-	    ab -c ${CL} -n ${NR} -k -e /logs/sample.csv http://${HOST}:${PORT}/${WEBSITE} > /logs/sample.out
+	    ab -c ${CL} -n ${NR} -k -e /logs/sample.csv http://${HOST}:${PORT}/${WEBSITE} > /logs/sample.out 2>>/logs/sample.err
 	elif [ "$BENCH" = "ab2" ]; then
-	    ab -c ${CL} -n ${NR} -e /logs/sample.csv http://${HOST}:${PORT}/${WEBSITE} > /logs/sample.out
+	    ab -c ${CL} -n ${NR} -e /logs/sample.csv http://${HOST}:${PORT}/${WEBSITE} > /logs/sample.out 2>>/logs/sample.err
 	elif [ "$BENCH" = "mysql1" ]; then
 	    sysbench --test=oltp --mysql-host=${HOST} \
 	    --mysql-port=${PORT} --oltp-table-size=${SIZE} \
 	    --mysql-db=test --mysql-user=admin --mysql-password=test1234 \
 	    --max-time=${TIME} --oltp-read-only=on --max-requests=${REQUESTS}  \
-	    --num-threads=${THREADS} run > /logs/sample.out
+	    --num-threads=${THREADS} run > /logs/sample.out 2>>/logs/sample.err
     elif [ "$BENCH" = "mysql2" ]; then
 	    sysbench --test=oltp --mysql-host=${HOST} \
 	    --mysql-port=${PORT} --oltp-table-size=${SIZE} \
 	    --mysql-db=test --mysql-user=admin --mysql-password=test1234 \
 	    --max-time=${TIME} --oltp-read-only=off --max-requests=${REQUESTS}  \
-	    --num-threads=8 run > /logs/sample.out
+	    --num-threads=8 run > /logs/sample.out 2>>/logs/sample.err
 	elif [ "$BENCH" = "cpu" ]; then
 	    sysbench --test=cpu --cpu-max-prime=${SIZE} --num-threads=8 \
-	    run > /logs/sample.out
+	    run > /logs/sample.out 2>>/logs/sample.err
 	elif [ "$BENCH" = "dacapo" ]; then
 		cat /xaa /xab > /dacapo.jar
-		java -jar /dacapo.jar ${CMD} >> /logs/sample.out
+		java -jar /dacapo.jar ${CMD} > /logs/sample.out 2>>/logs/sample.err
 	elif [ "$BENCH" = "io1" ]; then
 		sysbench --test=fileio \
 		--file-test-mode=rndrd --init-rng=on \
-		run > /logs/sample.out
+		run > /logs/sample.out 2>>/logs/sample.err
 	fi
 	elif [ "$BENCH" = "io2" ]; then
 		sysbench --test=fileio \
 		--file-test-mode=rndwr --init-rng=on \
-		run > /logs/sample.out
+		run > /logs/sample.out 2>>/logs/sample.err
 	fi
 done
--- docker-mysql/run.sh
+++ docker-mysql/run.sh
@@ -30,61 +30,21 @@ CreateMySQLUser()
 {
 	StartMySQL
 
-        #Setup DB
-        if [ "$ON_CREATE_DB" = "**False**" ]; then
-            unset ON_CREATE_DB
-        else
-            echo "Creating MySQL database ${ON_CREATE_DB}"
-            mysql -uroot -e "CREATE DATABASE IF NOT EXISTS ${ON_CREATE_DB};"
-            echo "Database created!"
-        fi
-
-	if [ "$MYSQL_PASS" = "**Random**" ]; then
-	    unset MYSQL_PASS
-	fi
-
-	PASS=${MYSQL_PASS:-$(pwgen -s 12 1)}
+    echo "Creating MySQL database ${ON_CREATE_DB}"
+    mysql -uroot -e "CREATE DATABASE IF NOT EXISTS ${ON_CREATE_DB};"
+    echo "Database created!"
+    	
+	PASS=${MYSQL_PASS}
 	_word=$( [ ${MYSQL_PASS} ] && echo "preset" || echo "random" )
 	echo "=> Creating MySQL user ${MYSQL_USER} with ${_word} password"
 
 	mysql -uroot -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '$PASS'"
 	mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION"
 
-
 	echo "=> Done!"
 
-	echo "========================================================================"
-	echo "You can now connect to this MySQL Server using:"
-	echo ""
-	echo "    mysql -u$MYSQL_USER -p$PASS -h<host> -P<port>"
-	echo ""
-	echo "Please remember to change the above password as soon as possible!"
-	echo "MySQL user 'root' has no password but only allows local connections"
-	echo "========================================================================"
-
 	mysqladmin -uroot shutdown
 }
-
-ImportSql()
-{
-	StartMySQL
-
-	for FILE in ${STARTUP_SQL}; do
-	   echo "=> Importing SQL file ${FILE}"
-	   mysql -uroot < "${FILE}"
-	done
-
-	mysqladmin -uroot shutdown
-}
-
-# Main
-if [ ${REPLICATION_MASTER} == "**False**" ]; then
-    unset REPLICATION_MASTER
-fi
-
-if [ ${REPLICATION_SLAVE} == "**False**" ]; then
-    unset REPLICATION_SLAVE
-fi
 
 # Initialize empty data volume and create MySQL user
 if [[ ! -d $VOLUME_HOME/mysql ]]; then
@@ -97,69 +57,9 @@ if [[ ! -d $VOLUME_HOME/mysql ]]; then
     echo "=> Done!"
     echo "=> Creating admin user ..."
     CreateMySQLUser
-else
-    echo "=> Using an existing volume of MySQL"
-fi
-
-# Import Startup SQL
-if [ -n "${STARTUP_SQL}" ]; then
-    if [ ! -f /sql_imported ]; then
-        echo "=> Initializing DB with ${STARTUP_SQL}"
-        ImportSql
-        touch /sql_imported
-    fi
-fi
-
-
-# Set MySQL REPLICATION - MASTER
-if [ -n "${REPLICATION_MASTER}" ]; then
-    echo "=> Configuring MySQL replication as master ..."
-    if [ ! -f /replication_configured ]; then
-        RAND="$(date +%s | rev | cut -c 1-2)$(echo ${RANDOM})"
-        echo "=> Writting configuration file '${CONF_FILE}' with server-id=${RAND}"
-        sed -i "s/^#server-id.*/server-id = ${RAND}/" ${CONF_FILE}
-        sed -i "s/^#log-bin.*/log-bin = mysql-bin/" ${CONF_FILE}
-        echo "=> Starting MySQL ..."
-        StartMySQL
-        echo "=> Creating a log user ${REPLICATION_USER}:${REPLICATION_PASS}"
-        mysql -uroot -e "CREATE USER '${REPLICATION_USER}'@'%' IDENTIFIED BY '${REPLICATION_PASS}'"
-        mysql -uroot -e "GRANT REPLICATION SLAVE ON *.* TO '${REPLICATION_USER}'@'%'"
-        echo "=> Done!"
-        mysqladmin -uroot shutdown
-        touch /replication_configured
-    else
-        echo "=> MySQL replication master already configured, skip"
-    fi
-fi
-
-# Set MySQL REPLICATION - SLAVE
-if [ -n "${REPLICATION_SLAVE}" ]; then
-    echo "=> Configuring MySQL replication as slave ..."
-    if [ -n "${MYSQL_PORT_3306_TCP_ADDR}" ] && [ -n "${MYSQL_PORT_3306_TCP_PORT}" ]; then
-        if [ ! -f /replication_configured ]; then
-            RAND="$(date +%s | rev | cut -c 1-2)$(echo ${RANDOM})"
-            echo "=> Writting configuration file '${CONF_FILE}' with server-id=${RAND}"
-            sed -i "s/^#server-id.*/server-id = ${RAND}/" ${CONF_FILE}
-            sed -i "s/^#log-bin.*/log-bin = mysql-bin/" ${CONF_FILE}
-            echo "=> Starting MySQL ..."
-            StartMySQL
-            echo "=> Setting master connection info on slave"
-            mysql -uroot -e "CHANGE MASTER TO MASTER_HOST='${MYSQL_PORT_3306_TCP_ADDR}',MASTER_USER='${MYSQL_ENV_REPLICATION_USER}',MASTER_PASSWORD='${MYSQL_ENV_REPLICATION_PASS}',MASTER_PORT=${MYSQL_PORT_3306_TCP_PORT}, MASTER_CONNECT_RETRY=30"
-            echo "=> Done!"
-            mysqladmin -uroot shutdown
-            touch /replication_configured
-        else
-            echo "=> MySQL replicaiton slave already configured, skip"
-        fi
-    else
-        echo "=> Cannot configure slave, please link it to another MySQL container with alias as 'mysql'"
-        exit 1
-    fi
-fi
 
 StartMySQL
 sysbench --test=oltp --oltp-table-size=1000000 --mysql-db=test --mysql-user=root  prepare
 mysqladmin -uroot shutdown
 
-tail -F $LOG &
-exec mysqld_safe
+tail -F $LOG & exec mysqld_safe
