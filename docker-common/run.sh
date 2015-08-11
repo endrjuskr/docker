db5c4a9ffabb7d1b07386c37d55046113f2f85b6
Andrzej Skrodzki <andrzej.skrodzki@gmail.com>
2015-08-11 17:16:55 +1000

    Fix local.

--- docker-common/run.sh
+++ docker-common/run.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
+chown www-data:www-data /app -R
+
 cd apache
 chmod +x create_apache.sh
 sh create_apache.sh 4 localhost
 
-sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2-1/apache2.conf
-a2enmod rewrite
-
-. /etc/apache2-1/envvars
+source /etc/apache2-1/envvars
 /usr/sbin/apache2ctl -d /etc/apache2-1/ -k start
-
-curl http://localhost:86/app1/index.php
 
 sleep infinity
