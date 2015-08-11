e437b8bb43a07b203d79fa9b324f7c58ebb2dda2
Andrzej Skrodzki <andrzej.skrodzki@gmail.com>
2015-08-11 17:14:21 +1000

    Fix local.

--- docker-common/run.sh
+++ docker-common/run.sh
@@ -4,6 +4,9 @@ cd apache
 chmod +x create_apache.sh
 sh create_apache.sh 4 localhost
 
+sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2-1/apache2.conf
+a2enmod rewrite
+
 . /etc/apache2-1/envvars
 /usr/sbin/apache2ctl -d /etc/apache2-1/ -k start
 
