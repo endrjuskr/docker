3d10adc7737992d76195f305f792c9d314736a35
Andrzej Skrodzki <andrzej.skrodzki@gmail.com>
2015-08-11 17:08:51 +1000

    Fix local.

--- docker-common/run.sh
+++ docker-common/run.sh
@@ -2,11 +2,11 @@
 
 cd apache
 chmod +x create_apache.sh
-sh create_apache.sh 4 10.240.162.123
+sh create_apache.sh 4 localhost
 
 . /etc/apache2-1/envvars
 /usr/sbin/apache2ctl -d /etc/apache2-1/ -k start
 
-curl http://10.240.162.123:86/app1/index.php
+curl http://localhost:86/app1/index.php
 
 sleep infinity
